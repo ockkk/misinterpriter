115c2a7fe48ac70fe1c5006642b522b63c9c2409
이현서 <hyeonseo@ihyeonseoui-MacBook-Pro.local>
2019-12-16 17:31:32 +0900

    :sparkles: Read md file with styled components Beta
    md 파일을 읽어오는 상세페이지 구현 베타버전입니다. styled-components 사용

--- misinterpriter/src/Pages/Detail/Detail.tsx
+++ misinterpriter/src/Pages/Detail/Detail.tsx
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
+import "github-markdown-css/github-markdown.css";
+import styled from "styled-components";
 
+const StyledReactMarkdown = styled(ReactMarkdown).attrs({
+  className: "markdown-body"
+})`
+  padding: 5px;
+  min-width: 300;
+  max-width: 100%;
+`;
 export const Detail: React.FC = () => {
-  const name = "hyunseo";
-  const postNum = 1;
+  const name = "jongok";
+  const postNum = 2;
 
   const [text, setText] = useState<string>("");
   useEffect(() => {
     async function getPosts() {
-      const mdFile = await fetch(
-        require(`Assets/hyunseo/${name}${postNum}.md`)
-      );
+      const mdFile = await fetch(require(`Assets/jongok/${name}${postNum}.md`));
       const Post = await mdFile.text();
+      console.log(Post);
       setText(Post);
     }
     getPosts();
   }, []);
 
   return (
-    <div style={{ fontWeight: "bold", width: "70%" }}>
-      <ReactMarkdown source={text}></ReactMarkdown>
+    <div style={{ fontWeight: "bold", width: "60%" }}>
+      <StyledReactMarkdown source={text}></StyledReactMarkdown>
     </div>
   );
 };
