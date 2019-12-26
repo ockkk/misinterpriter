ddf292538bf2721666096dc04780c5e4433ee560
황종옥 <jongokhwng@gmail.com>
2019-12-26 17:58:27 +0900

    :art: Modify js code

--- misinterpriter/src/Components/Board.tsx
+++ misinterpriter/src/Components/Board.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Card } from 'antd';
 import { Image } from './Boardstyle'
+import { Detail } from '../Pages/Detail/Detail'
+import { Match } from '@testing-library/dom';
 
 type Articleprops = {
-  title: string
-  name: string
+  data: string;
 }
 
-const Board: React.FC<Articleprops> = ({title, name}) => {
+const Board: React.FC<Articleprops> = ({data}) => {
   const { Meta } = Card;
+  const name = data.split("_")[0]
+  const title = data.split("_")[1]
+
   const handleClick = () => {
-    // <Detail name={"hyunseo"} postTitle={"hyunseo_5reason-to-use-graphql"} />
+    return <Detail/>
   }
   return (
 
         <Card
+          onClick={() => handleClick()}
           style={{"width":"98%"}}
         >
           <Image src="https://static.wixstatic.com/media/72c0b2_b309efbc0d03462a985e8f1aaa1995af~mv2.png/v1/fill/w_1064,h_608,al_c,q_90/72c0b2_b309efbc0d03462a985e8f1aaa1995af~mv2.webp"></Image>
