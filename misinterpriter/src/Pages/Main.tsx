0e40ba3b430cb72c7fb5b53d33f0392adc0fe86a
황종옥 <jongokhwng@gmail.com>
2019-12-26 18:00:01 +0900

    :art: Modify js code
    Detail 파일 props 받는 부분 수정하였습니다.

--- misinterpriter/src/Pages/Main.tsx
+++ misinterpriter/src/Pages/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 import Board from 'Components/Board'
 import articleData from '../Assets/articleData.json'
@@ -9,17 +9,34 @@ const Main: React.FC= () =>{
   var articleArr = Object.entries(articleData)
   const articleComponent:any = []
 
+  // const [articleArr, setarticleArr] = useState(Object.entries(articleData))
+
   articleArr.map((data) => {
     for(let i= 0; i< data[1].length; i++){
       articleComponent.push(
-        <Col span={6}>
-          <Link to={`/board`}>
-           <Board name={data[0]} title={data[1][i]}/>
+        <Col key={data[1][i]} span={6}>
+          <Link to={`/${data[0]}/${data[1][i]}`}>
+            <Board data={data[1][i]}/>
           </Link>
           <div style={{"margin":"10px"}}/>
         </Col>
         )
     }
+  })
+
+  useEffect(()=> {
+    // articleArr.map((data) => {
+    //   for(let i= 0; i< data[1].length; i++){
+    //     articleComponent.push(
+    //       <Col span={6}>
+    //         <Link to={`/${data[0] + data[1][i]}`}>
+    //         <Board name={data[0]} title={data[1][i]}/>
+    //         </Link>
+    //         <div style={{"margin":"10px"}}/>
+    //       </Col>
+    //       )
+    //   }
+    // })
   })
 
   return (
