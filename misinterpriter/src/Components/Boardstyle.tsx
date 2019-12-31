d5af737601af2cafe3fa916b3dccf03bd03de1c4
황종옥 <jongokhwng@gmail.com>
2019-12-31 12:11:00 +0900

    :lipstick: Modify board UI
    andt 디자인 카드 대신에 직접 board를 만들었습니다. 반응형이 안되긴 하는데 추후 적용해 보겠습니다.

--- misinterpriter/src/Components/Boardstyle.tsx
+++ misinterpriter/src/Components/Boardstyle.tsx
@@ -4,3 +4,28 @@ export const Image = sc.img`
   width: 250px;
   margin-bottom: 15px;
 `
+
+export const Image2 = sc.img`
+  border-radius: 0px 0px 3px 3px;
+  width: 95%;
+  height: 250px;
+`
+
+export const Title = sc.div`
+  border-bottom: solid 1px silver;
+  background-color: #9e9e9e08;
+  width: 95%;
+  height: 100px;
+  padding: 10px;
+  color: black;
+`
+
+export const Name = sc.div`
+  border-radius: 0px 0px 3px 3px;
+  background-color: #9e9e9e08;
+  width: 95%;
+  height: 30px;
+  padding: 3px;
+  padding-left: 10px;
+  color: black;
+`
