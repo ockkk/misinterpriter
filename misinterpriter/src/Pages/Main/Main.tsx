50b2f1d4c2bc5cad91a69356e9a013cb90db62ca
황종옥 <jongokhwng@gmail.com>
2020-01-20 18:49:59 +0900

    :art: Modify Main.js
    안쓰는 코드들 삭제 했습니다.

--- misinterpriter/src/Pages/Main/Main.tsx
+++ misinterpriter/src/Pages/Main/Main.tsx
@@ -1,28 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import Board from 'Components/Board/Board'
 import articleData from '../../Assets/articleData.json'
-import {Row, Col, Tag} from 'antd'
-import {Title} from './style'
+import {Row, Col} from 'antd'
 
 const Main: React.FC= () =>{
   var articledata = Object.entries(articleData)
-  const articleComponent:any = []
-  const TagName:Array<string> = []
-  const [articleArr, setarticleArr] = useState(articleComponent)
-
-  articledata.map((data) => {
-    for(let i= 0; i< data[1].length; i++){
-      articleComponent.push(
-        <Col key={data[1][i]["title"]} span={6}>
-            <Board data={data[1][i]} handleTag={handleTag}/>
-          <div style={{"margin":"20px"}}/>
-        </Col>
-        )
-      if(!TagName.includes(data[1][i]["category"])){
-        TagName.push(data[1][i]["category"])
-      }
+  const articleList:any = []
+  
+  for(let i in articledata){
+    for(let j=0; j < articledata[i][1].length; j++){
+      articleList.push(articledata[i][1][j])
     }
-  })
+  }
+  
+  let articleSlice: any = articleList.slice(0, 12);
+  let articleComponent: any = articleSlice.map((data: any) => {
+    return (
+      <Col key={data["title"]} span={6}>
+        <Board data={data} handleTag={handleTag} />
+        <div style={{ margin: "20px" }} />
+      </Col>
+    );
+  });
+
+  const [articleArr, setarticleArr] = useState(articleComponent)
+  const [length, setlength] = useState({"Preitems":0, "items":12})
+
+  const CallArticle = () => {
+    let temp: [JSX.Element];
+    temp = articleSlice.map((data: any) => {
+      return (
+        <Col key={data["title"]} span={6}>
+          <Board data={data} handleTag={handleTag} />
+          <div style={{ margin: "20px" }} />
+        </Col>
+      );
+    });
+    articleComponent = [...articleComponent, ...temp];
+    setarticleArr(articleComponent);
+  }
+
+  const handleReadMore = async() => {
+    if (articleList.length > length.items) {
+      setlength({ Preitems: length.items, items: length.items + 12 });
+    }
+  }
 
   function FilterArticle(tag:any){
     const filterArticleArr:any = []
@@ -30,9 +52,8 @@ const Main: React.FC= () =>{
       articledata[i][1].map(data => {
         if(data["category"] === tag){
           filterArticleArr.push(
-            <Col key={data["title"]} span={5}>
+            <Col key={data["title"]} span={6}>
               <Board data={data} handleTag={handleTag}/>
-              {/* <div style={{"margin":"20px"}}/> */}
             </Col>
           )
         }
@@ -45,26 +66,19 @@ const Main: React.FC= () =>{
     FilterArticle(e.target.innerHTML)
   }
 
-  useEffect( () => {
-    articledata.map((data) => {
-      for(let i= 0; i< data[1].length; i++){
-        articleComponent.push(
-          <Col key={data[1][i]["title"]} span={6}>
-              <Board data={data[1][i]} handleTag={handleTag}/>
-          </Col>
-          )
-        if(!TagName.includes(data[1][i]["category"])){
-          TagName.push(data[1][i]["category"])
-        }
-      }
-    })
-  },[])
+  useEffect(() => {
+    if (length.items !== 12) {
+      articleSlice = articleList.slice(length.Preitems, length.items);
+      CallArticle();
+    }
+  }, [length]);
+
   return (
     <div style={{padding:"10px", backgroundColor:"#f4f5f8"}}>
       <Row style={{paddingLeft:"100px", paddingRight:"100px"}}>
           {articleArr}
       </Row>
-      <button onClick={FilterArticle}>read more</button>
+      <button onClick={handleReadMore}>read more</button>
     </div>
   )
 }
